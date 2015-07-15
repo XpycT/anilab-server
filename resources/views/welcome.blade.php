542382b7b41a75c83cc2248fb24df2a8efadf72c
XpycT <XpycT.TOP@gmail.com>
2015-07-15 21:05:56 +0300

    --

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,7 +43,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello World ^_^</div>
+                <div class="title">AniLab</div>
             </div>
         </div>
     </body>
